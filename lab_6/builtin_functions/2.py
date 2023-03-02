08e16c98ead70c412f7dac489c1b4b4b46de3a22
Ulpan <123065911+ulpashk@users.noreply.github.com>
2023-03-02 16:38:18 +0600

    Delete lab_6/builtin_functions directory

--- lab_6/builtin_functions/2.py
+++
@@ -1,1 +0,0 @@
-#Write a Python program with builtin function that accepts a string and calculate the number of upper case letters and lower case letters
--- lab_6/builtin_functions/5.py
+++
@@ -1,4 +0,0 @@
-#Write a Python program with builtin function that returns True if all elements of the tuple are true.
-mylist = [True, True, True]
-x = all(mylist)
-print(x)
